395a03ee85ab9a5abd132235808707f60fd3e8aa
Yuvanjeyan <98009386+Yuvanjeyan@users.noreply.github.com>
2024-06-15 15:57:06 +0530

    Add files via upload

---
+++ JavaScript/love score.js
@@ -0,0 +1,6 @@
+var yourname=prompt("Enter your name : ");
+var crushname=prompt("Enter your crush name : ");
+
+var rnum=Math.random();
+rnum=Math.floor(rnum*100)+1;
+alert("The love relation between you two is "+rnum);
